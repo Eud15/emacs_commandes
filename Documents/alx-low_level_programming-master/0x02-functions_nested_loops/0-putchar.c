018a089b5bf38b700965784eeaf59ae1ab2ecd85
root <Eud15>
2022-04-07 00:58:43 +0200

    C programming at Alx school

---
+++ Documents/alx-low_level_programming-master/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - starts the program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	char string[] = "_putchar";
+	int ind = 0;
+
+		while (string[ind])
+			if (ind <= 9)
+			{
+				_putchar(string[ind]);
+				ind++;
+			}
+		_putchar('\n');
+		return (0);
+}
